a285c806546517131f199b3e94cfbee1fe4ed745
Christopher Regner <chrisregner.com@gmail.com>
2026-02-19 22:40:40 +0800

    Fix namespaces

--- ClientProject/ClientSource/Plugin.cs
+++ ClientProject/ClientSource/Plugin.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Barotrauma;
 
-namespace MyModName
+namespace PutShitInOnePlace
 {
     public partial class Plugin : IAssemblyPlugin
     {
--- SharedProject/SharedSource/Plugin.cs
+++ SharedProject/SharedSource/Plugin.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: IgnoresAccessChecksTo("DedicatedServer")]
 [assembly: IgnoresAccessChecksTo("BarotraumaCore")]
 
-namespace MyModName
+namespace PutShitInOnePlace
 {
     public partial class Plugin : IAssemblyPlugin
     {
